ce8046cb0e6f7beb510bf826ca11664055dced2e
Macx <Macx@macxdemacbook-pro.local>
2016-11-23 03:21:21 +0800

    2

---
+++ qq8059/qq8059/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qq8059
+//
+//  Created by Macx on 2016/11/23.
+//  Copyright © 2016年 Macx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qq8059/qq8059/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qq8059
+//
+//  Created by Macx on 2016/11/23.
+//  Copyright © 2016年 Macx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

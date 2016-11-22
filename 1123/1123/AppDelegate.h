8c75cc9c72281c56b2bf5cb5c9c463e532810232
Macx <Macx@macxdemacbook-pro.local>
2016-11-23 00:30:23 +0800

    init 001

---
+++ 1123/1123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1123
+//
+//  Created by Macx on 2016/11/23.
+//  Copyright © 2016年 Macx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
